8098852a46a22f6d9f81c24a770bff3424cf4cb6
紫川秀 <tszy111@126.com>
2018-05-17 16:29:02 +0800

    Initial Commit

---
+++ DisplayerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DisplayerView
+//
+//  Created by 紫川秀 on 2018/5/17.
+//  Copyright © 2018年 dangbei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
